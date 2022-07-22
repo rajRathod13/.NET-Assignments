366be20fb8583ef3337b10832e5a989b6056d8a6
Raj Rathod <r.k.rathod191@gmail.com>
2022-07-22 17:25:33 +0530

    Module1 task1 and task 2 done.

---
+++ Assignments/Module1/task1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que 1. What is C#");
+            Console.WriteLine("Ans 1. C# is general purpose, modern and object oriented programming language which pronounced as C sharp.");
+            Console.WriteLine();
+            Console.WriteLine("Que 2. Can we use keywords as an identifier? Why?");
+            Console.WriteLine("Ans 2. No we can not use keywords as identifier because keywords are predefined reserved words in C#.");
+            Console.Read();
+        }
+    }
+}
